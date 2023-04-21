203276535a52c047c2b0895ed6d70c910c445f57
Sunil <npatra412@gmail.com>
2023-04-21 10:33:40 +0530

    mod-7

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
-import AppleProvider from "next-auth/providers/apple"
+// import AppleProvider from "next-auth/providers/apple"
 import GoogleProvider from "next-auth/providers/google"
-import EmailProvider from "next-auth/providers/email"
+// import EmailProvider from "next-auth/providers/email"
 
 
 
@@ -10,27 +10,24 @@ export default NextAuth({
     secret: process.env.SECRET,
     providers: [
       // OAuth authentication providers
-      AppleProvider({
-        clientId: process.env.APPLE_ID,
-        clientSecret: process.env.APPLE_SECRET,
-      }),
+    //   AppleProvider({
+    //     clientId: process.env.APPLE_ID,
+    //     clientSecret: process.env.APPLE_SECRET,
+    //   }),
       GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
+        clientId: '82208393482-atn9b7fhiur6i9du30vaf0qv1lg7j8gv.apps.googleusercontent.com',
+        clientSecret: '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',
       }),
-      // Sign in with passwordless email link
-      EmailProvider({
-        server: process.env.MAIL_SERVER,
-        from: "<no-reply@example.com>",
-      }),
+      
+    
     ],
-  })
+  });
 
 
 
+  // 82208393482-atn9b7fhiur6i9du30vaf0qv1lg7j8gv.apps.googleusercontent.com
 
-
-
+  // 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
 
 
 //   signIn Page:-----------
